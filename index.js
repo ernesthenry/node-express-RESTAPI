fa3bbd8226c5b9f32621bab249d78363e610b22f
ernesthenry <henry38ernest@gmail.com>
2020-04-12 15:34:36 +0300

    Initial server set up

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from 'express'
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 4000;
+
+//mongoose connection 
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhostCRMdb', {
+    useNewUrlParser:true,
+    useUnifiedToplogy: true
+})
+
+//bodyParser setup
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 routes(app);
 
